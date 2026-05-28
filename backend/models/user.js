e6eefbc0c24c8f20bd04134c6fa0a82466124bcf
AryanGoyal111 <aryashree558@gmail.com>
2026-05-28 18:31:18 +0530

    Signin_up created

---
+++ backend/models/user.js
@@ -0,0 +1,33 @@
+const mongoose= require('mongoose');
+
+const userSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:6
+    },
+    role:{
+        type:String,
+        enum:['candidate','instructor','admin'],
+        default:'candidate'
+    },
+    isActive:{
+        type:Boolean,
+        default:true
+    }
+},{timestamps:true});
+
+const User=mongoose.model('user',userSchema);
+
+module.exports=User;
--- backend/routes/signin_up.js
+++ backend/routes/signin_up.js
@@ -1,21 +1,41 @@
 const express= require('express');
 
+const User= require('../models/user');
+
 const router=express.Router();
 
-router.route('/login')
-    .get((req,res)=>{
-        res.send("login is under process");
-    })
-    .post((req,res)=>{
-        res.send("login post is in under process");
+router.post('/login',async (req,res)=>{
+        const {email,password}=req.body;
+        if(!email||!password) res.status(400).json({message:"Fill all details"});
+        try{
+            const user=await User.findOne({email,password});
+            if(!user)
+                res.status(404).json({message:"Invalid Credentials"});
+            res.status(200).json(user);
+        }
+        catch(error){
+            res.status(500).json({message:error.message});
+        }
     });
 
-router.route('/signup')
-    .get((req,res)=>{
-        res.send('welcome new user to our under process signup page');
-    })
-    .post((req,res)=>{
-        res.send('signup post is in under process');
+router.post('/signup',async (req,res)=>{
+        const {name,email,password,role}=req.body;
+        if(!name||!email||!password) res.status(400).json({message:"Fill all details..."});
+        try{
+            const user=await User.create({
+                name,
+                email,
+                password,
+                role
+            });
+            res.status(201).json({
+                message:"User created Successfully",
+                user
+            });
+        }
+        catch(error){
+            res.status(500).json({message:error.message});
+        }
     });
 
 module.exports=router;
